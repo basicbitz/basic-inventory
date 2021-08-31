836ae6ea670f8de6f54144f2c18b000a86561914
BasicBitz <basicbitz@collateralgamage.com>
2021-08-31 00:51:16 -0500

    ability to add multiple UPCs, ability to exit from the loop

--- main.py
+++ main.py
@@ -8,21 +8,10 @@ import pprint
 import sqlite3
 
 api_url = 'https://www.pricecharting.com/api/product'
-upc='710425245763'
 
 # Game ID and name not needed, search by UPC unless UPC not available
 game_id='6910'
 game_name='Zelda II Link'
-
-
-# Sample UPCS
-# Zelda II NES
-# upc='045496630331'
-# Dragon Warrior NES
-# upc='045496630379'
-# Boogerman SEGA
-# upc='040421830183'
-# Scaler Gamecube
 
 # Create the DB
 conn = sqlite3.connect('inventory.db')
@@ -44,31 +33,46 @@ conn.execute('''CREATE TABLE IF NOT EXISTS GAMES
 # TODO: Allow decrement/removal if games are sold
 # TODO: look at sqlalchemy or peewee
 
-# Sample output
-# {'console-name': 'Super Nintendo', 'id': '6910', 'loose-price': 32906, 'product-name': 'EarthBound', 'status': 'success'}
+# Sample UPCS
+# Zelda II NES
+# upc='045496630331'
+# Dragon Warrior NES
+# upc='045496630379'
+# Boogerman SEGA
+# upc='040421830183'
+# Scaler Gamecube
+# upc='710425245763'
 
-# resp = requests.get(api_url, params={'t': api_config.token, 'id': game_id})
-# resp = requests.get(api_url, params={'t': api_config.token, 'q': game_name})
+upc=0 # init upc var
+while upc != "exit": # keep running until user types 'exit'
+    upc = input("Enter Video Game UPC Code: ") # get the UPC code from user
 
-resp = requests.get(api_url, params={'t': api_config.token, 'upc': upc})
-pp = pprint.PrettyPrinter(indent=4) 
-# pp.pprint(resp.headers)
-# pp.pprint(resp.json())
 
-game={} # initialize the game dict
-game['vgpc_id'] = resp.json()['id'] # Video Game Price Charting game ID
-game['product_name'] = resp.json()['product-name'] # product name
-game['console_name'] = resp.json()['console-name'] # name of console
-game['loose_price'] = resp.json()['loose-price'] # loose price of game
-qty=1 # this needs to be actual QTY (if new entry, set to 1 - increment each additional)
+    # Sample output
+    # {'console-name': 'Super Nintendo', 'id': '6910', 'loose-price': 32906, 'product-name': 'EarthBound', 'status': 'success'}
 
-pp.pprint(game)
+    # resp = requests.get(api_url, params={'t': api_config.token, 'id': game_id})
+    # resp = requests.get(api_url, params={'t': api_config.token, 'q': game_name})
+    if upc != "exit": # if 'upc' is not set to 'exit', let's get the game info and store it
+        resp = requests.get(api_url, params={'t': api_config.token, 'upc': upc})
+        pp = pprint.PrettyPrinter(indent=4) 
+        # pp.pprint(resp.headers)
+        # pp.pprint(resp.json())
 
-# for x in range(5000):
-#     conn.execute(f"INSERT INTO GAMES (VGPC_ID,PRODUCT_NAME,CONSOLE_NAME,LOOSE_PRICE,QTY,UPC) \
-#         VALUES ({game['vgpc_id']}, '{game['product_name']}', '{game['console_name']}', {game['loose_price']}, {qty}, {upc})" );
-conn.execute(f"INSERT INTO GAMES (VGPC_ID,PRODUCT_NAME,CONSOLE_NAME,LOOSE_PRICE,QTY,UPC) \
-    VALUES ({game['vgpc_id']}, '{game['product_name']}', '{game['console_name']}', {game['loose_price']}, {qty}, {upc})" );
+        game={} # initialize the game dict
+        game['vgpc_id'] = resp.json()['id'] # Video Game Price Charting game ID
+        game['product_name'] = resp.json()['product-name'] # product name
+        game['console_name'] = resp.json()['console-name'] # name of console
+        game['loose_price'] = resp.json()['loose-price'] # loose price of game
+        qty=1 # this needs to be actual QTY (if new entry, set to 1 - increment each additional)
+
+        pp.pprint(game)
+
+        # for x in range(5000):
+        #     conn.execute(f"INSERT INTO GAMES (VGPC_ID,PRODUCT_NAME,CONSOLE_NAME,LOOSE_PRICE,QTY,UPC) \
+        #         VALUES ({game['vgpc_id']}, '{game['product_name']}', '{game['console_name']}', {game['loose_price']}, {qty}, {upc})" );
+        conn.execute(f"INSERT INTO GAMES (VGPC_ID,PRODUCT_NAME,CONSOLE_NAME,LOOSE_PRICE,QTY,UPC) \
+            VALUES ({game['vgpc_id']}, '{game['product_name']}', '{game['console_name']}', {game['loose_price']}, {qty}, {upc})" );
 conn.commit()
 
 conn.close()
