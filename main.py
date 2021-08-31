8a71b9c3ec41038d4496e443a8b9fd542fde48c3
BasicBitz <basicbitz@collateralgamage.com>
2021-08-30 23:57:20 -0500

    cleanup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Need api_config.py file with 'token' set
+# Need api_config.py file with 'token' set for this to work
 import json
 import api_config
 import requests
@@ -8,8 +8,14 @@ import pprint
 import sqlite3
 
 api_url = 'https://www.pricecharting.com/api/product'
+upc='710425245763'
+
+# Game ID and name not needed, search by UPC unless UPC not available
 game_id='6910'
 game_name='Zelda II Link'
+
+
+# Sample UPCS
 # Zelda II NES
 # upc='045496630331'
 # Dragon Warrior NES
@@ -17,7 +23,6 @@ game_name='Zelda II Link'
 # Boogerman SEGA
 # upc='040421830183'
 # Scaler Gamecube
-upc='710425245763'
 
 # Create the DB
 conn = sqlite3.connect('inventory.db')
@@ -28,9 +33,16 @@ conn.execute('''CREATE TABLE IF NOT EXISTS GAMES
          VGPC_ID        INT     NOT NULL,
          PRODUCT_NAME   TEXT    NOT NULL,
          CONSOLE_NAME   TEXT    NOT NULL,
-         LOOSE_PRICE    INT     NOT NULL);''')
+         LOOSE_PRICE    INT     NOT NULL,
+         QTY            INT     NOT NULL,
+         UPC            INT);''')
 
-
+# TODO: Add UPC TABLE (write UPC to both tables)
+# TODO: Add readline/pause/scanner capability
+# TODO: Allow adding game based on name with confirmation/approval (verify proper game found on text search before saving)
+# TODO: Manage game quantity (allow increment QTY if game entry already exists in GAMES)
+# TODO: Allow decrement/removal if games are sold
+# TODO: look at sqlalchemy or peewee
 
 # Sample output
 # {'console-name': 'Super Nintendo', 'id': '6910', 'loose-price': 32906, 'product-name': 'EarthBound', 'status': 'success'}
@@ -43,18 +55,20 @@ pp = pprint.PrettyPrinter(indent=4)
 # pp.pprint(resp.headers)
 # pp.pprint(resp.json())
 
-game={}
-game['vgpc_id'] = resp.json()['id']
-game['product_name'] = resp.json()['product-name']
-game['console_name'] = resp.json()['console-name']
-game['loose_price'] = resp.json()['loose-price']
+game={} # initialize the game dict
+game['vgpc_id'] = resp.json()['id'] # Video Game Price Charting game ID
+game['product_name'] = resp.json()['product-name'] # product name
+game['console_name'] = resp.json()['console-name'] # name of console
+game['loose_price'] = resp.json()['loose-price'] # loose price of game
+qty=1 # this needs to be actual QTY (if new entry, set to 1 - increment each additional)
 
 pp.pprint(game)
 
-# look at sqlalchemy or peewee
-
-conn.execute(f"INSERT INTO GAMES (VGPC_ID,PRODUCT_NAME,CONSOLE_NAME,LOOSE_PRICE) \
-      VALUES ({game['vgpc_id']}, '{game['product_name']}', '{game['console_name']}', {game['loose_price']})" );
+# for x in range(5000):
+#     conn.execute(f"INSERT INTO GAMES (VGPC_ID,PRODUCT_NAME,CONSOLE_NAME,LOOSE_PRICE,QTY,UPC) \
+#         VALUES ({game['vgpc_id']}, '{game['product_name']}', '{game['console_name']}', {game['loose_price']}, {qty}, {upc})" );
+conn.execute(f"INSERT INTO GAMES (VGPC_ID,PRODUCT_NAME,CONSOLE_NAME,LOOSE_PRICE,QTY,UPC) \
+    VALUES ({game['vgpc_id']}, '{game['product_name']}', '{game['console_name']}', {game['loose_price']}, {qty}, {upc})" );
 conn.commit()
 
 conn.close()
