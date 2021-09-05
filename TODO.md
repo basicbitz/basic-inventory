c30b9c681669af2d4c1eedc6df0e7f5988bbd889
BasicBitz <basicbitz@collateralgamage.com>
2021-09-04 21:10:27 -0500

    CHANGED ALL THE THINGS

---
+++ TODO.md
@@ -0,0 +1,4 @@
+# TODO: Add UPC TABLE (write UPC to both tables)
+# TODO: Allow adding game based on name with confirmation/approval (verify proper game found on text search before saving)
+# TODO: Allow decrement/removal if games are sold
+# TODO: Handle `{'error-message': 'No such product', 'status': 'error'}` when UPC scanned is not a valid game UPC
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python
 
+# Game ID and name not needed, search by UPC unless UPC not available
+# game_name='Zelda II Link'
+
+# Sample UPCs for testing
+# Zelda II NES - upc = '045496630331'
+# Dragon Warrior NES - upc = '045496630379'
+# Boogerman SEGA - upc = '040421830183'
+# Scaler Gamecube - upc = '710425245763'
+# Doom 32X - upc = '010086845068'
+# Metal Head 32X - upc = '010086845112'
+
 # Need api_config.py file with 'token' set for this to work
 import json
 import api_config
@@ -7,10 +18,29 @@ import requests
 import pprint
 import datetime
 from peewee import *
+from playhouse.shortcuts import model_to_dict, dict_to_model
 
-def game_exists( upc, box_loc ):
-    query = Games.select().where((Games.upc == upc) & (Games.location == box_loc)) # Check if the UPC already exists in the DB
-    return(bool(query)) # is False if UPC does not exist in DB table, is True if UPC does already exist in DB table
+def game_inc(upc, loc):
+    increment_qty = Games.update({Games.qty: Games.qty + 1}).where((Games.upc == upc) & (Games.location == loc))
+    increment_qty.execute()
+
+def get_remote_game_data(upc):
+    # Sample output
+    # {'console-name': 'Super Nintendo', 'id': '6910', 'loose-price': 32906, 'product-name': 'EarthBound', 'status': 'success'}
+    resp = requests.get(api_url, params = {'t': api_config.token, 'upc': upc})
+    return resp.json()
+
+def game_create(game):
+    print("Creating entry for new game")
+    game = Games.create(**game)  
+    game.save()
+    return game.id
+
+def db_get_game(upc, loc):
+    return Games.select().where((Games.upc == upc) & (Games.location == loc))
+
+def db_get_game_id(id):
+    return Games.get_by_id(id)
 
 db = SqliteDatabase('inventory.db')
 
@@ -33,38 +63,9 @@ db.create_tables([Games])
 
 api_url = 'https://www.pricecharting.com/api/product'
 
-# Game ID and name not needed, search by UPC unless UPC not available
-# game_id='6910'
-# game_name='Zelda II Link'
+pp = pprint.PrettyPrinter(indent = 4)
 
-# Create the DB
-# conn = sqlite3.connect('inventory.db')
-# print("Opened database successfully")
-# conn.execute('''CREATE TABLE IF NOT EXISTS GAMES
-#          (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-#          VGPC_ID        INT     NOT NULL,
-#          PRODUCT_NAME   TEXT    NOT NULL,
-#          CONSOLE_NAME   TEXT    NOT NULL,
-#          LOOSE_PRICE    INT     NOT NULL,
-#          QTY            INT     NOT NULL,
-#          UPC            INT);''')
-
-# TODO: Add UPC TABLE (write UPC to both tables)
-# TODO: Allow adding game based on name with confirmation/approval (verify proper game found on text search before saving)
-# TODO: Allow decrement/removal if games are sold
-# TODO: Add column for box label
-# TODO: Handle `{'error-message': 'No such product', 'status': 'error'}` when UPC scanned is not a valid game UPC
-
-# Sample UPCS
-# Zelda II NES - upc = '045496630331'
-# Dragon Warrior NES - upc = '045496630379'
-# Boogerman SEGA - upc = '040421830183'
-# Scaler Gamecube - upc = '710425245763'
-# Doom 32X - upc = '010086845068'
-# Metal Head 32X - upc = '010086845112'
-
-upc = 0 # init upc var
-box_loc = None # init box location var - example, b1
+loc = None # init location var - example, b1 for box1, s3 for shelf 3
 while True: # keep running until user types 'exit'
     answer = input("Enter Video Game UPC Code, Box Location Code, or 'exit' to exit: ") # get the UPC code from user
 
@@ -72,40 +73,40 @@ while True: # keep running until user types 'exit'
         break
 
     if answer.startswith('b') or answer.startswith('s'):
-        box_loc = answer
+        loc = answer
         continue
 
-    if not box_loc:
+    if not loc:
         print("Box location must be set before scanning UPC codes!")
         continue
     
-    # query = Games.select().where(Games.upc == answer) # Check if the UPC already exists in the DB
-    # duplicate = bool(query) # is False if UPC does not exist in DB table, is True if UPC does already exist in DB table
-
-    if game_exists( answer, box_loc ): # if the UPC already exists in the DB table, let's increment the QTY
-        increment_qty = Games.update({Games.qty: Games.qty + 1}).where((Games.upc == answer) & (Games.location == box_loc))
-        increment_qty.execute()
+    game_id = None
+    if db_get_game(answer, loc): # if the UPC already exists in the DB table, let's increment the QTY
+        print("Duplicate game and location, incrementing qty")
+        game_inc(answer, loc)
+        for row in db_get_game(answer, loc):
+            game_id = row.id
     else: # if 'upc' is not set to 'exit', let's get the game info and store it
-        resp = requests.get(api_url, params = {'t': api_config.token, 'upc': answer})
-        pp = pprint.PrettyPrinter(indent = 4) 
+        print("Didn't find game")
         # pp.pprint(resp.headers)
         # pp.pprint(resp.json())
         
-        r = resp.json()
+        r = get_remote_game_data(answer)
+        # pp.pprint(r)
         
-        # Sample output
-        # {'console-name': 'Super Nintendo', 'id': '6910', 'loose-price': 32906, 'product-name': 'EarthBound', 'status': 'success'}
-        pp.pprint(r)
+        id = game_create({
+            'vgpc_id': r['id'],
+            'location': loc,
+            'product_name': r['product-name'], 
+            'console_name': r['console-name'],
+            'loose_price': r['loose-price'],
+            'upc': answer,
+            'qty': 1
+        })
 
-        game = Games.create(vgpc_id = r['id'],
-                            location = box_loc,
-                            product_name = r['product-name'], 
-                            console_name = r['console-name'],
-                            loose_price = r['loose-price'],
-                            upc = answer,
-                            qty = 1)
-        
-        game.save()
+        # pull it back out
+        game_id = id
+    
+    pp.pprint(model_to_dict(db_get_game_id(game_id)))
 
 db.close()
-
